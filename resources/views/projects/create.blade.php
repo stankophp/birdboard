f99734d08b06ebc6ea68b420948b73a33c246049
Stanko Markovic <stankophp@gmail.com>
2019-04-11 11:42:05 +0300

    wip

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -2,9 +2,6 @@
 
 @section('content')
 
-
-@include('flash::message')
-
 <form class="lg:w-1/2 lg:mx-auto bg-white p-6 md:py-12 md:px-16 rounded shadow"
       role="form" method="POST" action="/projects">
     <input type="hidden" name="_token" value="{{ csrf_token() }}">
--- resources/views/projects/edit.blade.php
+++ resources/views/projects/edit.blade.php
@@ -2,9 +2,6 @@
 
 @section('content')
 
-
-@include('flash::message')
-
 <div class="lg:w-1/2 lg:mx-auto bg-white p-6 md:py-12 md:px-16 rounded shadow">
     <h1 class="text-2xl font-normal mb-10 text-center">Edit Project</h1>
     <form role="form" method="POST" action="{{ $project->path() }}">
